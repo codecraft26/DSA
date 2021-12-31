b19bc40665da1c26baf1c2e7b387b575ba80e903
codescraft26 <codescraft26@gmail.com>
2022-01-01 01:36:25 +0530

    fourth commit

---
+++ leetcode/sortingthesentence.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+string sortSentence(string s ){
+    int n =s.size();
+}
+
+int main()
+{
+    string s;
+    cin>>s;
+    sortSentence(s);
+
+    
+    return 0;
+}
