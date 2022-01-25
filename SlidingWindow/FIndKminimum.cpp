63c8487834317a57157b9f0e0562b1bab43f1c98
codecraft26 <codescraft26@gmail.com>
2022-01-25 21:09:21 +0530

    window

--- SlidingWindow/FIndKminimum.cpp
+++ SlidingWindow/FIndKminimum.cpp
@@ -2,6 +2,22 @@
 using namespace std;
 
 int main(){
-
+    int n ,k;
+    cin>>n>>k;
+    vector<int> a(n);
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+int s=0,ans=INT16_MAX;
+for(int i=0;i<k;i++){
+    s+=a[i];
+}
+ans=min(s,ans);
+for(int i=1;i<n-1;i++){
+    s-=a[i-1];
+    s+=a[i+k-1];
+    ans=min(ans,s);
+}
+cout<<ans<<" ";
 return 0;
 }
