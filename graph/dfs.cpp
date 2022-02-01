5ee7f430157bbac1c4620e081b9d249a4bbfa6fa
codecraft26 <codescraft26@gmail.com>
2022-02-01 20:00:00 +0530

    graph

---
+++ graph/dfs.cpp
@@ -0,0 +1,18 @@
+#include "bits/stdc++.h"
+using namespace std;
+#define  vi vector<int> 
+#define vvi vector<vi>
+#define pii pair<int ,int>
+// #define vvi vector<pii>
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define ff first
+#define ss second;
+#define setBits(x) builtin_popcount(x)
+const int N=1e5+2,MOD=1e9+7;
+
+vi adj[N];
+
+signed main() 
+{
+    return 0;
+}
