60186d9a47d43f6fc735727f6ab609ef6fc38b4c
codecraft26 <codescraft26@gmail.com>
2022-03-06 10:26:29 +0530

    counting the frequency of string of character

---
+++ stringQuestion/CountTheFrequency.cpp
@@ -0,0 +1,26 @@
+
+//count the frequency in character
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+string s;
+cin>>s;
+char a;
+cin>>a;
+map<char , int >m;
+map<char , int >::iterator itr; // defining the iterator for map
+ for(long i=0;i<s.length();i++)
+        cout<<m[s[i]]++<<endl;
+        
+for(itr=m.begin();itr!=m.end();itr++)
+          
+            if(itr->first=='a'){
+                cout<<"this is freqquency"<<a<<endl;
+                cout<<itr->second; // this is for find the frequency of string
+            }
+
+
+return 0;
+}
