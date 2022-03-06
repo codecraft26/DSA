ee6d45a98396cf1d70ed0e46fabb78087013781e
codecraft26 <codescraft26@gmail.com>
2022-03-06 10:12:32 +0530

    arrange the file

--- Common question/maps2.cpp
+++ Common question/maps2.cpp
@@ -2,19 +2,17 @@
 using namespace std;
 // find the frequency of and element
 int main(){
-    int n ;
-    vector<int> a(n);
-    for (int i = 0; i < n; i++)
-    {
-        cin>>a[i];
-    }
+    int n =4;
+    vector<int> a{10,20,30,40,50};
+    
     
     map<int ,int> freq;
     for(int i=0;i<n;i++){
-        freq[a[i]]++;
-
+       cout<< freq[a[i]]++<<endl;
+        
 
     }
+        cout<<"end of freq[a[i]++]"<<endl;
     map<int,int> ::iterator it;
     for(it=freq.begin();it!=freq.end();it++){
         cout<<it->first<<" "<< it->second<<endl; 
