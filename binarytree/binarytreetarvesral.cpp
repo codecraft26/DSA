87c0669a66d1fb40f6105e7bca22764b0f751919
codecraft26 <codescraft26@gmail.com>
2022-03-13 12:45:25 +0530

    sum of all node and count the number of node in binnary tree

--- binarytree/binarytreetarvesral.cpp
+++ binarytree/binarytreetarvesral.cpp
@@ -6,12 +6,12 @@ struct Node{
    struct Node *left;
    struct Node * right;
    Node(int val){
-   data=val;
-   left=NULL;
-   right=NULL;
+        data=val;
+        left=NULL;
+        right=NULL;
  
-}
-};
+              }
+      };
 void preorder(Node *root){
   if(root==NULL){
     return;
@@ -28,6 +28,8 @@ void inorder( Node *root){
     cout<<root->data<<" ";
     inorder(root->right);
 }
+
+// function for count all the node in a binnary tree
 int getmax(Node *root){
   if(root==NULL){
     return 0;
@@ -35,14 +37,25 @@ int getmax(Node *root){
   return getmax(root->left)+getmax(root->right)+1;
 
 }
+
+//sum of all the node in binary tree 
+int sumofallnode(Node *root){
+if(root==NULL){
+  return 0;
+}
+else
+  return sumofallnode(root->left)+sumofallnode(root->right)+root->data;
+}
 int main() 
 {
   
     struct Node *root=new Node(1);
     root->left=new Node(3);
       root->left->left=new Node(3);
-        root->left->right=new Node(5);
-  
-   cout<< getmax(root);
+    root->left->right=new Node(5);
+ root->left->right->right=new Node(5);
+   cout<< getmax(root)<<endl;
+
+   cout<<sumofallnode(root)<<endl;
     return 0;
 }
