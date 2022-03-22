a5d9cbaf3a0718752f819bbe3b965f478c658847
codecraft26 <codescraft26@gmail.com>
2022-03-23 01:39:19 +0530

    find number of 1's in array

---
+++ Common question/find0sand1s.cpp
@@ -0,0 +1,17 @@
+
+//find the number of 1's without using if switch  and ternary operator 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int a[]={1,0,0,0,0,1,0,1,1,1};
+    int sum=0;
+    for (int i = 0; i<10; i++)
+    {
+            sum+=a[i];
+    }
+    cout<<"number of 1s is "<<sum<<endl;
+    
+
+return 0;
+}
