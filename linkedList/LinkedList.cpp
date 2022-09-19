510507a36cf85594adec2b6521cee4da1b223523
codecraft26 <codescraft26@gmail.com>
2022-09-19 19:57:41 +0530

    insertion and creation on github

---
+++ linkedList/LinkedList.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+using namespace std;
+ class Node {
+    public:
+        int data;
+        Node* next;
+        //constructor
+        Node(int data){
+            this->data=data;
+            this->next=NULL;
+
+        }
+        // new node create
+    void insertAtHead(Node* &head,int d ){
+
+        Node* temp=new Node(d);
+        temp->next=head;
+        head=temp;
+
+
+
+    }
+
+    
+ };
+  void insertAtHead(Node* &head,int d ){
+
+        Node* temp=new Node(d);
+        temp->next=head;
+        head=temp;
+
+
+
+    }
+    void printLinkedList(Node* &head){
+        Node* temp=head;
+        while(temp!=NULL){
+            cout<<temp->data<<endl;
+            temp=temp->next;
+            
+        }
+        cout<<endl;
+    
+
+    }
+
+int main(){
+    //create a new node 
+    Node* node1 = new Node(10);
+    
+
+    // cout<<node1->data<<endl;
+    // cout<<node1->next<<endl;
+    Node* head=node1;
+
+    insertAtHead(head,12);
+      insertAtHead(head,46);
+        insertAtHead(head,373);
+    printLinkedList(head);
+    cout<<endl;
+
+
+    
+
+return 0;
+}
