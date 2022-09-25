e660d14fee779e6aa679709e5105f2d26a989556
codecraft26 <codescraft26@gmail.com>
2022-09-26 02:20:10 +0530

    build tree from preorder and inorder

--- binarytree/preoderToInorder.cpp
+++ binarytree/preoderToInorder.cpp
@@ -1,5 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
+// preorder aur inorder se hme ek tree banani h okk 
 class Node{
    public:
         int data;
@@ -37,19 +38,55 @@ void postorder(Node* root){
     postorder(root->left);
     postorder(root->right);
     cout<<root->data<<endl;
+
+
+}
+int search(int inorder[],int start,int end ,int curr){
+    for(int i= start;i<=end;i++){
+        if(inorder[i]==curr){
+            return i;
+        }
+
+    }
+    return -1;
+
+}
+Node* buildTree(int preorder[],int inorder[],int start,int end ){
+    static int idx=0;
+    if(start>end){
+        return NULL;
+    }
+    int curr=preorder[idx];
+    idx++;
+    Node* node=new Node(curr);
+    if(start==end){
+        return node;
+    }
+    int pos=search(inorder,start,end,curr);
+    node->left=buildTree(preorder,inorder,start,pos-1);
+    node->right=buildTree(preorder,inorder,pos+1,end);
+    return node ;
+
+}
+void inorderprint(Node* root){
     
+    if(root==NULL){
+        return ;
+    }
+    inorderprint(root->left);
+    cout<<root->data<<" ";
+    inorderprint(root->right);
 
 }
 
+
+
 int main(){
     
-        Node* node1 = new Node(10);
-    Node* root=node1;
-    root->left=new Node(3);
-    root->left->right=new Node(34);
-    root->left->left=new Node(65);
-    root->right=new Node(5);
-    preorder(root);
+       int preorder[]={1,2,4,3,5};
+       int inorder[]={ 4,2,1,5,3};
+       Node* root= buildTree(preorder,inorder,0,4);
+       inorderprint(root);
 
 
 
