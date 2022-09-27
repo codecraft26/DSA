86413933ebbeca8d51ad3df75754556f0422993e
codecraft26 <codescraft26@gmail.com>
2022-09-27 15:01:47 +0530

    priority queue implementation

---
+++ Heaps/PriorityQueue.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    //maxheap
+    priority_queue<int,vector<int>> pq;
+    pq.push(2);
+    pq.push(3);
+
+    pq.push(5);
+    cout<<pq.top();
+    cout<<endl;
+    // min heap
+    priority_queue<int,vector<int>,greater<int> > am;
+    am.push(4);
+    am.push(4);
+    am.push(8);
+    cout<<am.top();
+
+
+
+
+return 0;
+}
