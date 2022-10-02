4623d9dc2815236a1f91f331fcb19b64fa5bf6c4
codecraft26 <codescraft26@gmail.com>
2022-10-02 14:40:08 +0530

    heapify algo

--- Heaps/ImplementationHeap.cpp
+++ Heaps/ImplementationHeap.cpp
@@ -66,8 +66,27 @@ class Heap{
 
 
     }
-};
 
+    //heapify algorthm 
+
+    
+};
+void heapify(int arr[],int n ,int i){
+    int largest=i;
+    int left=2*i;
+    int right=2*i+1;
+    if(left <n && arr[largest]<arr[left]){
+        largest=left;
+
+    }
+    if(right<n && arr[largest]<arr[right]){
+        largest=right;
+    }
+    if(largest!=i){
+        swap(arr[largest],arr[i]);
+        heapify(arr,n,largest);
+    }
+}
 int main(){
     Heap h;
     h.insert(45);
@@ -78,6 +97,15 @@ int main(){
     cout<<endl;
     h.deletion();
     h.Print();
-
+    cout<<endl;
+    int arr[6]={-1,54,53,55,52,50};
+    int n =5;
+    for(int i =n/2;i>0;i--){
+            heapify(arr,n,i);
+    }
+   cout<<"printing the array now";
+   for(int i =0;i<n;i++){
+    cout<<arr[i]<<" ";
+   }
 return 0;
 }
